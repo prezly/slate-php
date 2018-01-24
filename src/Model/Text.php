2ea4567fefd303315397d8c784d3b986b4c18f5b
e1himself <ivan.voskoboinyk@gmail.com>
2018-01-24 14:17:53 +0100

    Add a missing return type hint

--- src/Model/Text.php
+++ src/Model/Text.php
@@ -7,6 +7,9 @@ class Text implements Object
     /** @var Leaf[] */
     private $leaves = [];
 
+    /**
+     * @return \Prezly\Slate\Model\Leaf[]
+     */
     public function getLeaves(): array
     {
         return $this->leaves;
