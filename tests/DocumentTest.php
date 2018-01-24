a3695412ce66ef9d9f00b0900c5418803809f8b4
e1himself <ivan.voskoboinyk@gmail.com>
2018-01-25 00:01:39 +0100

    Implement a Document::getText() test case
    Code is not there yet

---
+++ tests/DocumentTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Prezly\Slate\Tests;
+
+class DocumentTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_should_get_concatenated_plain_text_content_of_inner_nodes()
+    {
+        $content = $this->loadContent('document_with_text.json');
+
+        $this->assertEquals(
+            "I'd like to introduce you to a very important person!",
+            $content->getDocument()->getText()
+        );
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,20 @@
 
 namespace Prezly\Slate\Tests;
 
+use Prezly\Slate\Model\Value;
+use Prezly\Slate\Unserializer;
+
 class TestCase extends \PHPUnit\Framework\TestCase
 {
     protected function loadFixture(string $fixture): string
     {
         return file_get_contents(__DIR__ . "/fixtures/$fixture");
+    }
+
+    protected function loadContent(string $filename): Value
+    {
+        $json = $this->loadFixture($filename);
+        $unserializer = new Unserializer();
+        return $unserializer->fromJSON($json);
     }
 }
