6c34ed821ae82874f5bf869a94b7943f93043826
e1himself <ivan.voskoboinyk@gmail.com>
2018-01-25 00:13:22 +0100

    Replace file path building strategy with path literals via __DIR__
    It's better because IDE supports Ctrl+Click on it.
    IDE better understands paths constants, but not buildable string values.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,8 @@ namespace Prezly\Slate\Tests;
 
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
-    protected function loadFixture(string $fixture): string
+    protected function loadFixture(string $file_path): string
     {
-        return file_get_contents(__DIR__ . "/fixtures/$fixture");
+        return file_get_contents($file_path);
     }
 }
