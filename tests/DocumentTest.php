e91d3a69bba26330f176908a7dd9ce7312049ed0
e1himself <ivan.voskoboinyk@gmail.com>
2018-01-25 11:27:38 +0100

    Reuse loadDocumentFromFixture

--- tests/DocumentTest.php
+++ tests/DocumentTest.php
@@ -8,11 +8,11 @@ class DocumentTest extends TestCase
      */
     public function it_should_get_concatenated_plain_text_content_of_inner_nodes()
     {
-        $content = $this->loadContentFromFixture(__DIR__ . '/fixtures/document_with_text.json');
+        $document = $this->loadDocumentFromFixture(__DIR__ . '/fixtures/document_with_text.json');
 
         $this->assertEquals(
             "I'd like to introduce you to a very important person!",
-            $content->getDocument()->getText()
+            $document->getText()
         );
     }
 }
