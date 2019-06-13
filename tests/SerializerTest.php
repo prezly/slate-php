c1d0d267ebec782b8f20126c66e202a6bf9a95fc
Ivan Voskoboinyk <ivan@prezly.com>
2019-06-13 16:50:24 +0300

    Add a test for Serializer class

---
+++ tests/SerializerTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace Prezly\Slate\Tests;
+
+class SerializerTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider fixtures
+     *
+     * @param string $file_path
+     */
+    public function it_should_serialize_to_json(string $file_path)
+    {
+        $json = $this->loadFixture($file_path);
+        $value = $this->loadContentFromFixture($file_path);
+
+        $json = implode("\n",
+            array_map(function (string $line): string {
+                return preg_replace('/^(\s+)/', '$1$1', $line);
+            }, explode("\n", $json))
+        );
+
+        $this->assertEquals(trim($json), $value->toJson(JSON_PRETTY_PRINT));
+    }
+
+    public function fixtures(): array
+    {
+        return [
+            'document_with_text.json' => [__DIR__ . '/fixtures/document_with_text.json'],
+        ];
+    }
+}
