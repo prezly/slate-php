9f328d2f3c3fe03dcf028bffaa97e83a1c35ee00
Ivan Voskoboinyk <ivan@prezly.com>
2019-06-20 00:34:39 +0300

    Re-start SerializerTest

--- tests/Serialization/SerializerTest.php
+++ tests/Serialization/SerializerTest.php
@@ -2,171 +2,52 @@
 namespace Prezly\Slate\Tests\Serialization;
 
 use InvalidArgumentException;
-use Prezly\Slate\Model\Block;
-use Prezly\Slate\Model\Inline;
-use Prezly\Slate\Model\Leaf;
-use Prezly\Slate\Model\Text;
+use Prezly\Slate\Model\Document;
+use Prezly\Slate\Model\Value;
 use Prezly\Slate\Serialization\Serializer;
 use Prezly\Slate\Tests\TestCase;
 
 class SerializerTest extends TestCase
 {
-    /** @var \Prezly\Slate\Serialization\Serializer */
-    private $serializer;
-
-    protected function setUp()
+    private function serializer(): Serializer
     {
-        parent::setUp();
-        $this->serializer = new Serializer();
-    }
-
-    /**
-     * @test
-     * @dataProvider valid_documents_jsons
-     *
-     * @param string $file_path
-     */
-    public function it_should_serialize_to_json(string $file_path)
-    {
-        $json = $this->loadFixture($file_path);
-        $value = $this->loadValueFromFixture($file_path);
-
-        $json = implode("\n",
-            array_map(function (string $line): string {
-                return preg_replace('/^(\s+)/', '$1$1', $line);
-            }, explode("\n", $json))
-        );
-
-        $this->assertEquals(trim($json), $value->toJson(JSON_PRETTY_PRINT));
-    }
-
-    /**
-     * The top level node of the Slate model is the Value. Any JSON that doesn't
-     * have a top-level document node is invalid
-     *
-     * @see https://docs.slatejs.org/slate-core/value
-     * @see https://docs.slatejs.org/slate-core/document
-     * @test
-     */
-    public function it_should_expect_top_level_document()
-    {
-        $this->expectException(InvalidArgumentException::class);
-        $this->serializer->fromJson("{}");
-    }
-
-    /**
-     * Since the Slate model always has a top-level node (the Value), the result of unserialization
-     * should always be a single Value instance
-     *
-     * @test
-     */
-    public function it_should_return_document_node()
-    {
-        $document = $this->loadDocumentFromFixture(__DIR__ . "/fixtures/empty_document.json");
-        $this->assertEmpty($document->getNodes());
+        return new Serializer();
     }
 
     /**
      * @test
      */
-    public function it_should_set_document_data()
+    public function it_should_serialize_value_to_json()
     {
-        $document = $this->loadDocumentFromFixture(__DIR__ . "/fixtures/document_with_data.json");
-        $this->assertEquals(["foo" => "bar"], $document->getData());
-    }
+        $value = new Value(new Document());
 
-    public function it_should_only_accept_block_nodes_for_document()
-    {
-        $fixture = $this->loadFixture(__DIR__ . "/fixtures/document_with_inline_children.json");
-        $this->expectException(InvalidArgumentException::class);
-        $this->serializer->fromJson($fixture);
+        $json = $this->serializer()->toJson($value);
+        $this->assertJson($json);
+
+        return $json;
     }
 
     /**
      * @test
+     * @depends it_should_serialize_value_to_json
+     * @param string $json
      */
-    public function it_should_add_children_to_document()
+    public function it_should_unserialize_value_from_json(string $json)
     {
-        $document = $this->loadDocumentFromFixture(__DIR__ . "/fixtures/document_with_flat_children.json");
-        $nodes = $document->getNodes();
+        $value = new Value(new Document());
 
-        $this->assertEquals(3, count($nodes));
-        foreach ($nodes as $node) {
-            $this->assertInstanceOf(Block::class, $node);
-        }
-
-        $this->assertEquals("paragraph", $nodes[0]->getType());
-        $this->assertEquals("blockquote", $nodes[1]->getType());
-        $this->assertEquals("list", $nodes[2]->getType());
+        $this->assertEquals($value, $this->serializer()->fromJson($json));
     }
+
 
     /**
      * @test
      */
-    public function it_should_nest_children()
+    public function it_should_store_version_upon_serialization()
     {
-        $document = $this->loadDocumentFromFixture(__DIR__ . "/fixtures/document_with_nested_children.json");
-        $children = $document->getNodes();
+        $json = $this->serializer()->toJson(new Value(new Document()));
 
-        // Second-level children
-        $this->assertEquals(2, count($children[0]->getNodes()));
-        $this->assertEquals(0, count($children[1]->getNodes()));
-        $this->assertEquals(2, count($children[2]->getNodes()));
-    }
-
-    /**
-     * @test
-     */
-    public function it_should_load_document_with_leaves()
-    {
-        $document = $this->loadDocumentFromFixture(__DIR__ . "/fixtures/document_with_text.json");
-
-        $this->assertCount(1, $document->getNodes());
-        $block = $document->getNodes()[0];
-
-        /** @var Block $block */
-        $this->assertInstanceOf(Block::class, $block);
-        $this->assertEquals('paragraph', $block->getType());
-        $this->assertCount(3, $block->getNodes());
-
-        /** @var Text $text */
-        $text = $block->getNodes()[0];
-        $this->assertInstanceOf(Text::class, $text);
-        $this->assertCount(3, $text->getLeaves());
-
-        $expected_texts = [
-            "I'd like to introduce ",
-            'you',
-            ' to a ',
-        ];
-
-        foreach ($text->getLeaves() as $i => $leaf) {
-            $this->assertInstanceOf(Leaf::class, $leaf);
-            $this->assertEquals($expected_texts[$i], $leaf->getText());
-        }
-    }
-    /**
-     * @test
-     */
-    public function it_should_set_node_data()
-    {
-        $document = $this->loadDocumentFromFixture(__DIR__ . "/fixtures/nodes_with_data.json");
-        $block = $document->getNodes()[0];
-        $this->assertEquals(["foo" => "bar"], $block->getData());
-
-        /** @var Inline $inline */
-        $inline = $block->getNodes()[0];
-        $this->assertEquals(["name" => "John Doe", "id" => 1234], $inline->getData());
-    }
-
-    /**
-     * @test
-     */
-    public function it_should_set_mark_data()
-    {
-        $document = $this->loadDocumentFromFixture(__DIR__ . "/fixtures/mark_with_data.json");
-        $leaf = $document->getNodes()[0]->getNodes()[0]->getLeaves()[0];
-        $this->assertEquals(["href" => "/foo"], $leaf->getMarks()[0]->getData());
+        $this->assertArrayHasKey('version', json_decode($json, true));
     }
 
     /**
@@ -176,11 +57,11 @@ class SerializerTest extends TestCase
      * @param string $file
      * @param string $expected_error
      */
-    public function it_should_fail_deserializing_invalid_structure(string $file, string $expected_error)
+    public function it_should_fail_unserializing_invalid_structure(string $file, string $expected_error)
     {
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage($expected_error);
-        $this->loadDocumentFromFixture($file);
+        $this->serializer()->fromJson($this->loadFixture($file));
     }
 
     public function valid_documents_jsons(): array
@@ -191,7 +72,7 @@ class SerializerTest extends TestCase
     }
 
     /**
-     * @see it_should_fail_deserializing_invalid_structure
+     * @see it_should_fail_unserializing_invalid_structure
      */
     public function invalid_documents_fixtures()
     {
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,26 +2,10 @@
 
 namespace Prezly\Slate\Tests;
 
-use Prezly\Slate\Model\Document;
-use Prezly\Slate\Model\Value;
-use Prezly\Slate\Serialization\Serializer;
-
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
     protected function loadFixture(string $file_path): string
     {
         return file_get_contents($file_path);
-    }
-
-    protected function loadValueFromFixture(string $file_path): Value
-    {
-        $json = $this->loadFixture($file_path);
-        $serializer = new Serializer();
-        return $serializer->fromJson($json);
-    }
-
-    protected function loadDocumentFromFixture(string $file_path): Document
-    {
-        return $this->loadValueFromFixture($file_path)->getDocument();
     }
 }
